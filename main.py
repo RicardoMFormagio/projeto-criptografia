ae547826f8cb3027cab929210787c14f4cc3ea9c
Ricardo M. Formagio <morais.formagio@hotmail.com>
2026-03-23 16:26:35 -0300

    Remove large videos and update project

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+import time
+
 from crip_simetrica import gerar_e_salvar_chave, criptografar_arquivo, decriptografar_arquivo
 from crip_assimetrica import decriptografar_rsa, gerar_chaves_rsa, criptografar_rsa
 from utils import gerar_hash_arquivo
 import logging
+from esteganografia import revelar_arquivo_de_imagem,  ocultar_arquivo_em_imagem
 
 # Configuração do Log
-logging.basicConfig(filename='operacoes.log', level=logging.INFO, format='%(asctime)s - %(message)s')
+logging.basicConfig(
+    filename='operacoes.log', 
+    level=logging.INFO, 
+    format='%(asctime)s.%(msecs)03d | %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
 def exibir_menu():
     print("\n-*-*-*-*-*-*-*-* SISTEMA DE SEGURANÇA -*-*-*-*-*-*-*-*")
@@ -15,6 +23,9 @@ def exibir_menu():
     print("4. Gerar Par de Chaves RSA (Pública/Privada)")
     print("5. Criptografar Arquivo (Assimétrica - RSA)")
     print("6. Decriptografar Arquivo (Assimétrica)")
+    print("--------------------------------------------------")
+    print("7. Ocultar Arquivo em Imagem (Esteganografia)")
+    print("8. Revelar Arquivo de Imagem (Esteganografia)")
     print("--------------------------------------------------")
     print("0. Sair")
     print("--------------------------------------------------")
@@ -35,23 +46,35 @@ while True:
         print(f"Hash do arquivo original: {hash_antes}")
         logging.info(f"HASH ARQUIVO ORIGINAL: {hash_antes}")
 
+        start_crip = time.perf_counter()
         criptografar_arquivo(arquivo)
+        end_crip = time.perf_counter()
+        t_crip = end_crip - start_crip
+        
         print("Arquivo criptografado com sucesso!")
-        logging.info(f"ARQUIVO CRIPTOGRAFADO (SIMETRICA): {arquivo}.enc")
+        logging.info(f"ARQUIVO CRIPTOGRAFADO (SIMETRICA): {arquivo}.enc | Tempo de execucao {t_crip:.6f} s")
 
     elif opcao == "3":
         arquivo_encriptado = input("Caminho do arquivo encriptado '.enc': ")
+        
+        start_decrip = time.perf_counter()
         arquivo_decriptado = decriptografar_arquivo(arquivo_encriptado)
+        end_decrip = time.perf_counter()
+        t_decrip = end_decrip - start_decrip
+        
         print("Arquivo decriptado!")
-        logging.info(f"ARQUIVO DECRIPTADO (SIMETRICA): {arquivo_decriptado}")
+        logging.info(f"ARQUIVO DECRIPTADO (SIMETRICA): {arquivo_decriptado} | Tempo de execucao {t_decrip:.6f} s")
 
         hash_depois = gerar_hash_arquivo(arquivo_decriptado)
         print(f"Hash do arquivo decriptado: {hash_depois}")
         logging.info(f"HASH ARQUIVO DECRIPTADO (SIMETRICA): {hash_depois}\n\n")
 
     elif opcao == "4":
+
         gerar_chaves_rsa()
+
         print("Par de chaves RSA gerado com sucesso!")
+
         logging.info("Par de chaves RSA gerado com sucesso!")
 
     elif opcao == "5": 
@@ -61,20 +84,72 @@ while True:
         print(f"Hash do arquivo original: {hash_antes}")
         logging.info(f"HASH ARQUIVO ORIGINAL: {hash_antes}")
 
+        start_rsa = time.perf_counter()
         criptografar_rsa(arquivo)
+        end_rsa = time.perf_counter()
+        t_rsa = end_rsa - start_rsa
+
         print("Arquivo criptografado com a sua Chave Pública!")
-        logging.info(f"ARQUIVO CRIPTOGRAFADO (ASSIMETRICA): {arquivo}.rsa")
+        logging.info(f"ARQUIVO CRIPTOGRAFADO (ASSIMETRICA): {arquivo}.rsa | Tempo de execucao {t_rsa:.6f} s")
 
     elif opcao == "6": 
         arquivo_rsa = input("Caminho do arquivo '.rsa': ")
-        # Captura o nome gerado pela função RSA
+        
+        start_dec_rsa = time.perf_counter()
         arquivo_decriptado = decriptografar_rsa(arquivo_rsa)
+        end_dec_rsa = time.perf_counter()
+        t_dec_rsa = end_dec_rsa - start_dec_rsa
+
         print("Arquivo decriptado!")
-        logging.info(f"ARQUIVO DECRIPTADO (ASSIMETRICA): {arquivo_decriptado}")
+        logging.info(f"ARQUIVO DECRIPTADO (ASSIMETRICA): {arquivo_decriptado} | Tempo de execucao {t_dec_rsa:.6f} s")
 
         hash_depois = gerar_hash_arquivo(arquivo_decriptado)
         print(f"Hash do arquivo decriptado: {hash_depois}")
         logging.info(f"HASH ARQUIVO DECRIPTADO (ASSIMETRICA): {hash_depois} \n\n")
+
+    elif opcao == "7":
+        img_origem = input("Caminho da imagem base (ex: foto.png): ")
+        arquivo_esconder = input("Caminho do arquivo para ocultar: ")
+        
+        hash_interno = gerar_hash_arquivo(arquivo_esconder)
+        logging.info(f"INICIANDO ESTEGANOGRAFIA - HASH ARQUIVO ORIGINAL: {hash_interno}")
+        
+<<<<<<< HEAD
+        start_esteg = time.perf_counter()
+        imagem_final = ocultar_arquivo_em_imagem(img_origem, arquivo_esconder)
+        end_esteg = time.perf_counter()
+        t_esteg = end_esteg - start_esteg
+        
+        print(f"Sucesso! Arquivo ocultado em: {imagem_final}")
+        logging.info(f"ESTEGANOGRAFIA CONCLUIDA: {imagem_final} | Tempo de execucao {t_esteg:.6f} s")
+=======
+        imagem_final = ocultar_arquivo_em_imagem(img_origem, arquivo_esconder)
+        
+        print(f"Sucesso! Arquivo ocultado em: {imagem_final}")
+        logging.info(f"ESTEGANOGRAFIA CONCLUIDA: {imagem_final}")
+>>>>>>> bf78365630040db905b58545bd8ad1a4a613ed2d
+
+    elif opcao == "8":
+        img_secreta = input("Caminho da imagem secreta: ")
+        
+<<<<<<< HEAD
+        start_rev = time.perf_counter()
+        arquivo_extraido = revelar_arquivo_de_imagem(img_secreta)
+        end_rev = time.perf_counter()
+        t_rev = end_rev - start_rev
+        
+        print(f"Arquivo extraído com sucesso: {arquivo_extraido}")
+        logging.info(f"ARQUIVO EXTRAIDO DA IMAGEM: {arquivo_extraido} | Tempo de execucao {t_rev:.6f} s")
+=======
+        arquivo_extraido = revelar_arquivo_de_imagem(img_secreta)
+        
+        print(f"Arquivo extraído com sucesso: {arquivo_extraido}")
+        logging.info(f"ARQUIVO EXTRAIDO DA IMAGEM: {arquivo_extraido}")
+>>>>>>> bf78365630040db905b58545bd8ad1a4a613ed2d
+        
+        hash_extraido = gerar_hash_arquivo(arquivo_extraido)
+        print(f"Hash do arquivo extraído: {hash_extraido}")
+        logging.info(f"HASH ARQUIVO EXTRAIDO (ESTEGANOGRAFIA): {hash_extraido}\n\n")
         
     elif opcao == "0":
         break
